57ec0296c41ec22b369353160cb5db8e9a236954
stefOuroboros <stephane.gallois89@gmail.com>
2018-11-06 12:00:34 +0100

    first commit, commit before clean then bootstrap

---
+++ JS/scrolling.js
@@ -0,0 +1,16 @@
+//basic initialisation
+    var controller = new ScrollMagic.Controller({globalSceneOptions: {
+            triggerHook: 'onLeave'
+        }
+    });
+
+    var slides = document.querySelectorAll("section.panel");
+
+    for (var i=0; i<slides.length; i++) {
+        new ScrollMagic.Scene({
+            triggerElement: slides[i]
+        })
+        .setPin(slides[i])
+        .addIndicators()
+        .addTo(controller);
+        }
